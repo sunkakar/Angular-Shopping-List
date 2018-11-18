6ac5b4cdf74c5369ff96b0a17ee7a20be0cf5621
sunkakar <sundeepkakar@gmail.com>
2018-11-18 14:38:18 -0500

    Added new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from 'src/app/header/header.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Shopping-List';
+  title = 'Shopping List';
 }
